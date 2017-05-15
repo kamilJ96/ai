12d77f3335496f1b540aaf6bd838039508481238
AilinhT <linh@LinhDebian>
2017-05-15 15:21:22 +1000

    random move bot

---
+++ ai/RandomBot.java
@@ -0,0 +1,77 @@
+package bleh;
+
+/* Kamil Jakrzewski kjakrzewski
+ * Ai-Linh Tran taal */
+
+import aiproj.slider.*;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class RandomBot implements SliderPlayer{
+	private char player;
+	private char opponent;
+	
+	private PersonalBoard b;
+
+	private final int WEIGHT_PCES_RMVD = 10;
+	private final int WEIGHT_PCES_END = 2;
+	
+	@Override
+	public void init(int dimension, String board, char player) {
+		this.player = player;
+		this.b = new PersonalBoard(board, dimension);
+		
+		if(player == 'H'){
+			this.opponent = 'V';
+		}
+		else{
+			this.opponent = 'H';
+		}
+	}
+
+	//update move w.r.t other player
+	@Override
+	public void update(Move move) {
+		if(move == null)
+			return;
+		
+		PersonalMoves m = PersonalMoves.toPersonalMoves(move);
+		Integer[] pos = {move.i, move.j};
+		
+		// Only update board if opponent didn't pass
+		b.updateBoard(pos, opponent, m);
+	}
+
+	//the strategy
+	@Override
+	public Move move() {
+		return randomMove();
+	}
+	
+	private Move randomMove() {
+		ArrayList<Integer[]> myPieces = this.b.getPieces(player);
+		int rndMove = ThreadLocalRandom.current().nextInt(0, 3);
+		int rndPiece = ThreadLocalRandom.current().nextInt(0, myPieces.size());
+		Police check = new Police(myPieces.get(rndPiece)[0], myPieces.get(rndPiece)[1], b);
+		
+		Move m;
+		
+		if(player == 'H' && check.hCheck(PersonalMoves.H_MOVES[rndMove])){
+			b.updateBoard(myPieces.get(rndPiece), player, PersonalMoves.H_MOVES[rndMove]);
+		}
+		else if(player == 'V' && check.hCheck(PersonalMoves.V_MOVES[rndMove])){
+			b.updateBoard(myPieces.get(rndPiece), player, PersonalMoves.V_MOVES[rndMove]);
+		}
+		else {
+			randomMove();
+		}
+		
+		m = PersonalMoves.H_MOVES[rndMove].toMove(myPieces.get(rndPiece), PersonalMoves.H_MOVES[rndMove]);
+		return m;
+	}
+	
+	
+		
+	
+}
